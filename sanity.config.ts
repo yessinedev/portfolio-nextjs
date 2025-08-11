6de512d669cde850417a915318047c7278a68a60
yessinedev <yessine.ag@gmail.com>
2025-08-11 01:45:38 +0100

    docs(sanity): remove outdated config comment
    The comment was removed as it no longer accurately describes the configuration's usage since the Studio route has changed.

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,8 +1,5 @@
 "use client";
 
-/**
- * This configuration is used to for the Sanity Studio that’s mounted on the `\app\studio\[[...tool]]\page.tsx` route
- */
 
 import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
